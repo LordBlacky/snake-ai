51ae24a14657349e2a42eac29e2eca4af143de9f
LordBlacky <93870579+LordBlacky@users.noreply.github.com>
2025-08-07 18:25:30 +0200

    Add Sigmoid ActivationFunction

--- src/model/Activation/ActivationFunctions.py
+++ src/model/Activation/ActivationFunctions.py
@@ -38,3 +38,23 @@ class Tanh:
 
     def backward(self, error_tensor):
         return error_tensor * (1 - np.tanh(self.intermediate_value) ** 2)
+
+
+class Sigmoid:
+
+    def __init__(self):
+        self.intermediate_value = None
+
+    def sigmoid(self, input_tensor):
+        return 1 / (1 + np.exp(-input_tensor))
+
+    def forward(self, input_tensor):
+        self.intermediate_value = input_tensor
+        return self.sigmoid(input_tensor)
+
+    def backward(self, error_tensor):
+        return (
+            error_tensor
+            * self.sigmoid(self.intermediate_value)
+            * (1 - self.sigmoid(self.intermediate_value))
+        )
